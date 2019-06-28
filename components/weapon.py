c9e348bd82eedf00134169fa9421ae71462d17ff
networkingguru <runningones@gmail.com>
2019-06-28 13:19:00 -0500

    Full brawling attacks added

--- components/weapon.py
+++ components/weapon.py
@@ -62,11 +62,18 @@ class Unarmed(Weapon):
         self.base_ap = 0
         #self, name, attack_mod, parry_mod, stamina, b_dam, s_dam, p_dam, t_dam, hands, damage_type, base_ap, 
         #hand = True, length = 0, side_restrict = True, restricted_locs = [], restricted_angles = []
-        self.punch = Attack('Punch', 0, 40, 1, .5, 0, 0, 0, 1, 'B', 20,)
-        self.kick = Attack('Kick', -20, 0, 2, .75, 0, 0, 0, 0, 'B', 30, False)
         self.jab = Attack("Jab/Cross", 0, 0, 1, .5, 0, 0, 0, 1, 'B', 10, True, 0, True, [0], [8], [8])
         self.haymaker = Attack("Haymaker", -20, +20, 3, .7, 0, 0, 0, 1, 'B', 25, True, 0, True, [27,28], [2,3,4], [5,6,7])
-        self.base_attacks = [self.jab, self.haymaker]
+        self.hook = Attack("Hook", -10, +10, 2, .6, 0, 0, 0, 1, 'B', 20, True, 0, True, [27,28], [2,3,4], [5,6,7])
+        self.uppercut = Attack("Uppercut", -20, 0, 2, .6, 0, 0, 0, 1, 'B', 20, True, 0, True, [2,3,4,7,8,11,12,15,16,19,20,21,22,23,24,25,26,27,28], [3,4], [5,4])
+        self.hammerfist = Attack("Hammer Fist", 0, -20, 1, .4, 0, 0, 0, 1, 'B', 10, True, 0, True, [2,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28], [7,0,1], [7,0,1])
+        self.elbow = Attack("Elbow Strike", -20, -20, 1, .6, 0, 0, 0, 1, 'B', 10, True, -14, True, [], [0,1,2,3], [0,7,6,5])
+        self.frontkick = Attack("Front Kick", -10, +10, 2, .6, 0, 0, 0, 1, 'B', 15, False, 0, False, [0,27,28], [8], [8])
+        self.roundhousekick = Attack("Roundhouse Kick", +10, +20, 5, .8, 0, 0, 0, 1, 'B', 30, False, 0, False, [0,27,28], [2,3], [5,6])
+        self.sidekick = Attack("Side Kick", -20, +10, 3, .7, 0, 0, 0, 1, 'B', 20, False, 0, False, [0,27,28], [8], [8])
+        self.stomp = Attack("Stomp", +20, 0, 1, .4, 0, 0, 0, 1, 'B', 10, False, 0, False, [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26], [0], [0])
+        self.knee = Attack("Knee Strike", -30, -10, 1, .4, 0, 0, 0, 1, 'B', 15, False, -18, False, [0,2,27,28], [3,4], [4,5])
+        self.base_attacks = [self.jab, self.haymaker, self.hook, self.uppercut, self.hammerfist, self.elbow, self.frontkick, self.roundhousekick, self.sidekick, self.stomp, self.knee]
         self.attacks = []
 
 
